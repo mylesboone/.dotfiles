dfbd6b9ddb5d1c3d3ea8bf899ba3d9c9d28e067c
Myles Boone <mylesboone@gmail.com>
2022-06-27 11:18:53 -0400

    Add build release function

---
+++ zsh/.zsh/functions/build_release.zsh
@@ -0,0 +1,7 @@
+build_release() {
+  if [[ $# > 0 ]]; then
+    wf --approved-prs | fzf -m | cut -d' ' -f1 | tr '\n' ',' | xargs wf --build-release --list $1 --pull-requests
+  else
+    echo "argument required (release name)"
+  fi
+}
