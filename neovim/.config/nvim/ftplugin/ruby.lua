17dd7f2c0023008f5fc7db41a41bce610c08f16b
Myles Boone <mylesboone@gmail.com>
2025-04-01 18:58:23 -0400

    smarter run spec

--- neovim/.config/nvim/ftplugin/ruby.lua
+++ neovim/.config/nvim/ftplugin/ruby.lua
@@ -1,12 +1,15 @@
+local buffer_map = vim.api.nvim_buf_set_keymap
+
 vim.opt_local.iskeyword:append("!")
 vim.opt_local.iskeyword:append("?")
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>c', ":Econtroller<CR>", { noremap = true, desc = 'open controller' })
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>m', ":Emodel<CR>", { noremap = true, desc = 'open model' })
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>v', ":Eview<CR>", { noremap = true, desc = 'open view' })
+buffer_map(0, 'n', '<leader>c', ":Econtroller<CR>", { noremap = true, desc = 'open controller' })
+buffer_map(0, 'n', '<leader>m', ":Emodel<CR>", { noremap = true, desc = 'open model' })
+buffer_map(0, 'n', '<leader>v', ":Eview<CR>", { noremap = true, desc = 'open view' })
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>.', "<Plug>RailsOpenAlt", { noremap = true, desc = 'open alt' })
+buffer_map(0, 'n', '<leader>.', "<Plug>RailsOpenAlt", { noremap = true, desc = 'open alt' })
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>rr', ":call RunNearestSpec()<CR>", { noremap = true, desc = 'nearest spec' })
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>rf', ":call RunCurrentSpecFile()<CR>", { noremap = true, desc = 'current file' })
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>rl', ":call RunLastSpec()<CR>", { noremap = true, desc = 'last spec' })
+buffer_map(0, 'n', '<leader>rr', ":lua _G.tests.rspec.run_nearest()<CR>", { noremap = true, desc = 'nearest test' })
+buffer_map(0, 'n', '<leader>rf', ":lua _G.tests.rspec.run_file()<CR>", { noremap = true, desc = 'current file' })
+buffer_map(0, 'n', '<leader>ra', ':lua _G.tests.rspec.run_all()<CR>', { noremap = true, desc = 'all files' })
+buffer_map(0, 'n', '<leader>rl', ":lua _G.tests.run_last()<CR>", { noremap = true, desc = 'last test' })
---
+++ neovim/.config/nvim/lua/tests.lua
@@ -0,0 +1,74 @@
+local function get_filename()
+  local cwd = vim.fn.getcwd()
+  local full_path = vim.fn.expand('%:p')
+  local relative_path = full_path:gsub("^" .. cwd:gsub("[%^%$%(%)%%%.%[%]%*%+%-%?]", "%%%0") .. "/", "")
+
+  return relative_path
+end
+
+local function remove_until(original_string, until_string)
+  local start_position, end_position = original_string:find(until_string)
+
+  if start_position then
+    return original_string:sub(start_position)
+  else
+    return original_string
+  end
+end
+
+local function get_line_number()
+  local line, column = unpack(vim.api.nvim_win_get_cursor(0))
+  return line
+end
+
+local go = {
+  run_all = function()
+    vim.cmd("VimuxRunCommand('go test ./...')")
+  end,
+  run_file = function()
+    local filename = get_filename()
+
+    local directory = filename:match("(.*/)")
+
+    if directory ~= "./" then
+      directory = directory:sub(1, #directory - 1)
+    end
+
+    vim.cmd('VimuxRunCommand("go test ./' .. directory .. '")')
+  end,
+}
+
+local js = {
+  run_all = function()
+    vim.cmd("VimuxRunCommand('yarn test')")
+  end,
+  run_file = function()
+    local filename = get_filename()
+    vim.cmd('VimuxRunCommand("yarn test ./' .. filename .. '")')
+  end,
+}
+
+local rspec = {
+  run_all = function()
+    vim.cmd("VimuxRunCommand('bundle exec rspec')")
+  end,
+  run_file = function()
+    local filename = remove_until(get_filename(), "spec")
+    vim.cmd('VimuxRunCommand("bundle exec rspec ./' .. filename .. '")')
+  end,
+  run_nearest = function()
+    local filename = remove_until(get_filename(), "spec")
+    vim.cmd('VimuxRunCommand("bundle exec rspec ./' .. filename .. ':' .. get_line_number() .. '")')
+  end,
+}
+
+local function run_last()
+  vim.cmd("VimuxRunLastCommand")
+end
+
+return {
+  go = go,
+  js = js,
+  rspec = rspec,
+  run_last = run_last,
+}
