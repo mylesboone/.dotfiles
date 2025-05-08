24bec84d6b30616d35fee91a5af6a18e266da0d6
Myles Boone <mylesboone@gmail.com>
2025-05-08 10:13:07 -0400

    increase timeout for rubocop

--- neovim/.config/nvim/lsp/rubocop.lua
+++ neovim/.config/nvim/lsp/rubocop.lua
@@ -6,7 +6,7 @@ return {
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       callback = function()
-        vim.lsp.buf.format({ bufnr = bufnr })
+        vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 5000 })
       end,
     })
   end,
